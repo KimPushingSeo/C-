39439aba607ea6162d2b0c417a4c60ec5b8c9a97
KimPushingSeo <157454841+KimPushingSeo@users.noreply.github.com>
2024-01-31 18:41:28 +0900

    Add files via upload

---
+++ 두들낙서 9강 종합문제.cpp
@@ -0,0 +1,12 @@
+﻿//두 숫자를 입력 받아서 그 숫자들의 합을 출력하는 프로그램
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	int a, b;
+	printf("더할 숫자 두 개를 입력해주세요 : ");
+	scanf("%d %d", &a, &b);
+	int sum = a + b;
+	printf("%d\n", a, b, sum);
+}
