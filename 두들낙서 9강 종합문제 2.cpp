392d1b3d4376df2e20df7811580afe2729f31053
KimPushingSeo <157454841+KimPushingSeo@users.noreply.github.com>
2024-01-31 18:42:44 +0900

    Add files via upload

---
+++ 두들낙서 9강 종합문제 2.cpp
@@ -0,0 +1,18 @@
+﻿//BMI = w/t**2
+//체중(kg,실수)과 키(m,실수)를 입력받아서 체질량 지수를 구하는 프로그램
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+
+	float w,t;
+
+	printf("체중을 입력해 주세요 (kg) :");
+	scanf("%f", &w);
+	printf("키를 입력해 주세요 (m) :");
+	scanf("%f", &t);
+	double bmi = w / (t * t);
+	printf("BMI : %f ", bmi);
+
+}
---
+++ 두들낙서 9강 종합문제 3.cpp
@@ -0,0 +1,12 @@
+﻿//알파벳을 입력 받아서 그 다음 알파벳을 출력하는 프로그램을 만들어 보세요.
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	char a; //a는 문자야
+	printf("알파벳을 입력해주세요 : ");
+	scanf("%c", &a); //입력된 문자를 a에 저장해
+	int b = (int)a + 1;//a를 잠깐 정수로 생각하고 1더한건 정수 b야
+	printf("%c 다음 알파벳은 %c입니다", a, b); //b를 문자로 출력해
+}
